dbbabfdc25363f8423e713bc772f0c9e0a9577bc
Edgar <ednicprz!gmail.com>
2020-11-05 23:49:03 -0400

    Faltan las estrellas

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,15 +2,16 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import 'bootstrap'
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery')
-require('packs/custom')
-require('packs/posts.js')
-
+    //require('packs/custom')
+    //require('packs/posts.js')
+require("raty-js")
+require('webpack-dev-server')
+require("packs/jquery.raty")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -21,14 +22,10 @@ require('packs/posts.js')
 //= require bootstrap-sprockets
 //= require bootstrap-sprockets
 //= require bootstrap
+//= require webpack
 
-//= require jquery_ujs
-//= require jquery.raty
-//= require jquery3
-//= require jquery3
-//= require jquery_ujs
 //= require_tree .
-//= require_tree ../../../vendor/javascript/packs
+//= require_tree 
 
 
 //= require jquery-ui
